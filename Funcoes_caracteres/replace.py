398d330b108dd88352fcaec2e85b81c7c6ead688
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-23 16:48:16 -0300

    Atualizar

---
+++ Funcoes_caracteres/replace.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conexao = sqlite3.connect("caracter.db")
+cursor = conexao.cursor()
+
+cursor.execute('''
+    SELECT nome,REPLACE(nome, 'a', 'A') AS nome_modificado
+    FROM USERS
+    WHERE LENGTH(nome) > 10;
+''')
+
+consulta = cursor.fetchall()
+for i in consulta:
+    print(i)
+
+conexao.commit()
+conexao.close()
