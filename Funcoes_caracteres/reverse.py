fce4f9cf7fcf66eab4b7e682e39c9a1da911bfb1
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-23 16:49:05 -0300

    Atualizar

---
+++ Funcoes_caracteres/reverse.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+conexao = sqlite3.connect("caracter.db")
+
+
+cursor = conexao.cursor()
+
+
+cursor.execute('''
+            SELECT REVERSE(nome)
+            FROM USERS
+            WHERE SUBSTR(nome,1,4) = 'Arth';        
+''')
+
+consulta = cursor.fetchall()
+for i in consulta:
+    print(i)
+
+conexao.commit()
+conexao.close()
