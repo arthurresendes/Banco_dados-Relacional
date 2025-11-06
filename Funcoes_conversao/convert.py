52818e05a01cdc4c002e5af903d60a5b384f71db
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-06 13:43:53 -0300

    Atualizar

---
+++ Funcoes_conversao/convert.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+
+conexao = sqlite3.connect("banco.db")
+cursor = conexao.cursor()
+
+# Nao funciona no sqlite , apenas demonstração
+cursor.execute("""
+        SELECT nome, CONVERT(FLOAT , vendas_qtd), valor_venda, round((vendas_qtd * valor_venda),2) AS total FROM VENDEDOR
+""")
+res = cursor.fetchall()
+for i in res:
+    print(i)
+
+conexao.commit()
+conexao.close()
