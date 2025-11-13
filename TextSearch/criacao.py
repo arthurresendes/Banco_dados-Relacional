6ba17a9df8c8102597412b75b041cb037248f9c5
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-13 17:45:55 -0300

    atualização

---
+++ TextSearch/criacao.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+conexao = sqlite3.connect("banco.db")
+cursor = conexao.cursor()
+
+
+cursor.execute("""
+        CREATE TABLE VENDEDOR(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome VARCHAR(200),
+            nome_venda VARCHAR(100),
+            vendas_qtd INTEGER,
+            valor_venda REAL
+        );
+""")
+
+conexao.commit()
+conexao.close()
