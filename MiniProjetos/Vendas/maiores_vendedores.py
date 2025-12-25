286a310ba443117db569e6fe6cd1888beea52084
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-12-25 18:13:14 -0300

    Querry maiores vendedores

--- MiniProjetos/Vendas/maiores_vendedores.py
+++ MiniProjetos/Vendas/maiores_vendedores.py
@@ -5,8 +5,12 @@ conexao = sqlite3.connect("banco.db")
 cursor = conexao.cursor()
 
 cursor.execute("""
-    SELECT v.nome,v.nome_venda, v.vendas_qtd * v.valor_venda
-    FROM VENDEDOR as v
+    SELECT v.nome,c.email,v.nome_venda, v.vendas_qtd * v.valor_venda, ve.data_venda
+    FROM VENDA as ve
+    JOIN CLIENTE c
+    ON ve.id_cliente = c.id 
+    JOIN VENDEDOR v
+    ON ve.id_vendedor = v.id 
     ORDER BY v.vendas_qtd * v.valor_venda DESC
     LIMIT 5
 """)
